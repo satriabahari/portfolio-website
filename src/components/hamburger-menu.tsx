88d29ad41bfd4eabe243f1228f0f5b51e239d2ef
Satria Bahari <satriaabaharii@gmail.com>
2023-09-11 21:31:37 +0700

    Menambahkan menu hamburger dan pop

--- src/components/hamburger-menu.tsx
+++ src/components/hamburger-menu.tsx
@@ -1,4 +1,6 @@
 import React, { useState } from 'react'
+import PopMenu from "./pop-menu";
+import { RxHamburgerMenu, RxCross2 } from "react-icons/rx";
 
 export default function HamburgerMenu() {
   const [isActive, setIsActive] = useState(false)
@@ -9,11 +11,17 @@ export default function HamburgerMenu() {
 
   return (
     <section className='flex md:hidden'>
-      <div className='flex flex-col justify-center items-center cursor-pointer' onClick={handleClick}>
-        <span className={`block dark:bg-gray-50 bg-gray-800 w-6 h-1 rounded-sm origin-top-right transition ${isActive ? "-rotate-[46deg]" : null }`}></span>
-        <span className={`block dark:bg-gray-50 bg-gray-800 w-7 h-1 rounded-sm my-1 origin-right transition ${isActive ? "w-0" : null }`}></span>
-        <span className={`block dark:bg-gray-50 bg-gray-800 w-6 h-1 rounded-sm origin-bottom-right transition ${isActive ? "rotate-[46deg]" : null }`}></span>
-      </div>
+      <button className='text-gray-800 dark:text-white' onClick={() => handleClick()}>
+        {isActive ?
+          (
+            <React.Fragment>
+              <RxCross2 size={35} />
+              <PopMenu />
+            </React.Fragment>
+          )
+          : <RxHamburgerMenu size={35} />
+        }
+      </button>
     </section>
   )
 }
---
+++ src/components/pop-menu.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { links } from "@/lib/data";
+import Link from 'next/link';
+
+export default function PopMenu() {
+  return (
+    <section className='md:hidden z-50 relative'>
+      <nav className="fixed left-0 right-0 top-14 py-8 h-screen flex items-center justify-center text-center bg-gray-50 dark:bg-gray-800">
+        <ul className='flex flex-col gap-y-8'>
+          {links.map((link) => (
+            <li key={link.hash} className='text-xl text-gray-600 hover:text-gray-950 dark:text-gray-400 dark:hover:text-gray-50'>
+              <Link href={link.hash}>{link.name}</Link>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </section>
+  )
+}
